ee359b256b5210937065739f5f16513168727f0e
John Chiaramonte <chiaramonte.j@northeastern.edu>
2022-03-07 23:10:34 -0500

    Making YoloSubscriber into TrashYoloSubscriber

--- yolo_subscriber.py
+++ yolo_subscriber.py
@@ -12,9 +12,9 @@ from darknet_ros_msgs.msg import *
     - /darknet_ros/bounding_boxes : darknet_ros_msgs/BoundingBoxes
 
 '''
-class YoloSubscriber():
-    def __init__(self):
-        rospy.init_node('YoloSubscriber')
+class TrashYoloSubscriber():
+    def __init__(self, TrashSpottedCallback = None):
+        #rospy.init_node('TrashYoloSubscriber')
 
         # PUBLISHERS
 
@@ -22,19 +22,52 @@ class YoloSubscriber():
         rospy.Subscriber('/darknet_ros/found_object', ObjectCount, self.DarknetFoundObjectReceivedEvent)
         rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.DarknetBoundingBoxesReceivedEvent)
 
+        # Keeps track of all trash within the current frame
+        # type: List<TrashPiece>
+        self.trash = []
+        # Keeps track of how many pieces are within frame
+        self.count = 0
+
+        # The callback function which we will execute when we see trash
+        self.TrashSpottedCallbackFunction = TrashSpottedCallback
+
+        # Dimensions of Camera
+        self.frame_width = 640
+        self.frame_height = 480
+    
     def DarknetFoundObjectReceivedEvent(self, data):
-        #print('found trash lol')
-        #print(data)
-        print()
+        self.count = data.count
 
     def DarknetBoundingBoxesReceivedEvent(self, data):
         print('got a bounding box for trash')
         print(data)
-        self.trash = data
+        # clear the trash list
+        self.trash *= 0
+
+        # Add all trash to the list of currently spotted trash
+        for bounding_box in data:
+            self.trash.append(TrashPiece(data))
+
+        if self.TrashSpottedCallbackFunction != None:
+            self.TrashSpottedCallbackFunction(self.trash)
+
+
+class TrashPiece():
+    def __init__(self, bounding_box):
+        # Confidence that this trash is actually trash
+        self.confidence = bounding_box.probability
+
+        # Bounding box coordinates
+        self.x_bounds = (bounding_box.x_min, bounding_box.x_max)
+        self.y_bounds = (bounding_box.y_min, bounding_box.y_max)
+        
+        # Centered coordinates
+        self.x = self.x_bounds[1] - ((self.x_bounds[1] - self.x_bounds[0]) / 2)
+        self.y = self.y_bounds[1] - ((self.y_bounds[1] - self.y_bounds[0]) / 2)
 
 
 if __name__ == '__main__':
-    subscribe = YoloSubscriber()
+    subscribe = TrashYoloSubscriber()
 
     while True:
         continue
