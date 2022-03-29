2dd8bcdfa2e345c8b9dbcd4b6e1a6c9b7f7d7785
Capstone Lab <chiaramonte.j@northeastern.edu>
2022-03-29 19:13:47 -0400

    Two trash test script

--- test_bot.py
+++ test_bot.py
@@ -3,17 +3,34 @@ import rospy
 
 if __name__ == '__main__':
     t = TrashBot()
-    print('STARTING UP THE TRASHBOT')
-    t.StartUp()
-    print('WAITING 10 SECONDS')
-    rospy.sleep(10)
-    print('STOPPING THE TRASHBOT')
-    t.ShutDown()
-    print('WAITING 5 SECONDS')
-    rospy.sleep(5)
-    print('STARTING UP TRASHBOT')
-    t.StartUp()
-    print('WAITING 5 SECONDS THEN SHUTTING DOWN')
-    rospy.sleep(5)
-    t.ShutDown()
 
+
+    # start up the trashbot and pay attention to YOLO
+    t.StartUp()
+    print('Waiting until pickup')
+
+    # pause until we aren't responding to trash anymore
+    # ie: the robot has found and picked up a piece of trash
+    while t.respond_to_trash:
+        rospy.sleep(0.1)
+    print('PIECE 1 picked up (hopefully)')
+    
+    print('TURNING AROUND')
+
+    # turn around (this goes to a defined angle, just for testing)
+    t.kobuki_base.turn_to_angle(-0.9)
+
+    # start up the bot again
+    t.StartUp()
+
+    print('WAITING FOR ANOTHER DETECTION')
+
+    # pause until we aren't responding to trash anymore
+    # ie: the robot has found and picked up a piece of trash
+    while t.respond_to_trash:
+        rospy.sleep(0.1)
+
+    print('PIECE 2 picked up (hopefully)')
+    print('Sleeping for 5 seconds and exiting')
+
+    rospy.sleep(5)
