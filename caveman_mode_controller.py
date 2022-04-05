1a82a89b24152cf1ea886684429018b799d2a6f7
Capstone Lab <chiaramonte.j@northeastern.edu>
2022-04-04 20:14:10 -0400

    Added PickUpTrash to ModeController

--- caveman_mode_controller.py
+++ caveman_mode_controller.py
@@ -1,6 +1,7 @@
 from trash_bot import *
 from trash_mapper import *
 from anti_cluster import *
+from vespucci import *
 import rospy
 
 # Controls caveman mode modes
@@ -14,6 +15,7 @@ class ModeController():
         self.trash_bot = TrashBot()
         self.trash_mapper = TrashMapper()
         self.anti_cluster = AntiCluster(1.0,1.0, False)
+        self.navigator = Vespucci()
 
     def StartMapper(self):
         print("ModeController: Starting Mapper")
@@ -36,8 +38,43 @@ class ModeController():
 
     def Halt(self):
         print("ModeController: Halting")
+        self.navigator.ShutDown()
         self.trash_mapper.StopListeningToYolo()
         self.trash_bot.ShutDown()
+
+    def PickUpTrash(self):
+        refresh_rate = 10 #Hz
+        caveman_time_limit = 10 (seconds)
+        r = rospy.Rate(refresh_rate)
+        dt = 0
+
+        print('ModeController: Attempting to pick up trash')
+
+        # while there are still points to go to
+        while(self.navigator.HasPoints()):
+            # go near the closest trash point
+            self.navigator.GoNearClosestPose()
+
+            # start up the TrashBot
+            dt = 0
+            self.StartTrashBot()
+
+            # while the bot is running and time elapsed hasnt breached our limit,
+            # wait
+            # the trashbot should toggle `trash_bot.running` if it successfully
+            # finds and attacks a piece of trash and shuts down, otherwise we 
+            # just observe the time limit and manually shut trashbot down
+            while(self.trash_bot.running and dt < caveman_time_limit):
+                dt += 1.0/float(refresh_rate)
+                r.sleep()
+
+            if dt >= caveman_time_limit:
+                self.trash_bot.ShutDown()
+
+            
+        print('ModeController: Done picking up trash i hope')
+            
+
 
 if __name__ == '__main__':
     m = ModeController(True)
